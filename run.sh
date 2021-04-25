61a165c69ac2fdd774492e53c05ef375cf2c94df
shiran dudy <shirandudy@pop-os.localdomain>
2021-04-25 10:14:46 -0600

    rename lattice inflection FST

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 set -x
 set -e
 
-dev_fname="rus.dev" # make sure is correct
-letter_fname="russian" # make sure is found
-isyms="symbols" # will be created
-lattice="inflection.fst" # will be created
-query_file="query.txt" # an example file is found
+dev_fname=rus.dev # make sure is correct
+letter_fname=russian # make sure is found
+isyms=symbols # will be created
+query_file=query.txt # an example file is found
 constraint="V;FUT;1;SG" # set prior condition (V;FUT;1;SG)
+lattice=${letter_fname}_${constraint}.fst # will be created
 
 # create a symbol system
 python src/create_syms.py $dev_fname $letter_fname $isyms
